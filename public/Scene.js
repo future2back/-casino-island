924e11b5bb63d97af9564cd45d06b04c75e09cae
future2back <74167741+future2back@users.noreply.github.com>
2023-08-14 03:36:37 -0300

    Update Scene.js

--- public/Scene.js
+++ public/Scene.js
@@ -1,15 +1,52 @@
-// Example Scene.js
-
 class ExternalScene extends window.BaseScene {
   constructor() {
     super({
+      name: "test_island",
       player: {
-       // The x/y coordinates on your Map where you want the player to appear
         spawn: {
           x: 210,
           y: 280,
         },
       },
+      // map: {
+      //   tilesetUrl: "http://localhost:3002/tilesheet.png",
+      // },
+      // mmo: {
+      //   enabled: true,
+      //   url: "ws://localhost:2567",
+      //   roomId: "community_island",
+      // },
     });
+  }
+
+  create() {
+    super.create();
+
+    const npcClothing = {
+      body: "Goblin Potion",
+      hat: "Pirate Hat",
+      hair: "Buzz Cut",
+      shirt: "SFL T-Shirt",
+      pants: "Farmer Pants",
+      tool: "Pirate Scimitar",
+    };
+
+    this.initialiseNPCs([
+      {
+        x: 240,
+        y: 320,
+        npc: "geoff",
+        clothing: npcClothing,
+        onClick: () => {
+          window.openModal({
+            npc: {
+              name: "Geoff",
+              clothing: npcClothing,
+            },
+            jsx: "Howdy farmer! Welcome to the test community island, stay safe out there.",
+          });
+        },
+      },
+    ]);
   }
 }
